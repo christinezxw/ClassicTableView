6b3148803da86ba1095e60e8d5deceb459423f00
Xuanwei Zhang <christinezxw@hotmail.com>
2021-09-16 20:53:08 -0700

    check code for classic table view

--- ClassicTableView/ViewController.swift
+++ ClassicTableView/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tblView: UITableView!
+    let arr = ["Aa", "Bb", "Cc", "Dd"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tblView.delegate = self
+        tblView.dataSource = self
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return how many celles wanted
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = arr[indexPath.row]
+        return cell
+    }
+    
 }
 
